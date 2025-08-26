ca363fa1d7e5210412aa01cf1d9990e3998e7ff8
Sander Teunissen <s.teunissen@gmail.com>
2025-08-26 08:26:58 +0200

    feat: add installation script

---
+++ scripts/install/install.sh
@@ -0,0 +1,6 @@
+#AI! create a bash script inside this file which uses brew package manager to install packages.
+# - Put the packages to be installed into two collections: "terminal" and "system".
+# - when I run "install.sh full" I want group "terminal" and "system" to be installed.
+# - when I run "install.sh terminal" I want only the group "terminal" to be installed.
+# - packages in "terminal": (fzf, git-delta, starship, tlrc)
+# - packages in "system": (kanata, )
