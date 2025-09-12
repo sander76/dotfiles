fbe77e1c49dda393644bf9eae505acca7027f20f
Sander Teunissen <s.teunissen@gmail.com>
2025-09-12 08:36:00 +0200

    install aider

---
+++ config/install_aider.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+PARENT_FOLDER="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+echo "$PARENT_FOLDER"
+
+"$PARENT_FOLDER/prepare.sh"
+"$PARENT_FOLDER/install_uv.sh"
+
+uv tool install --force --python python3.12 --with pip aider-chat@latest
+
+[ ! -e ~/.aider.conf.yml ] && ln -s "$PARENT_FOLDER/aider_config/.aider.conf.yml"  ~/.aider.conf.yml
