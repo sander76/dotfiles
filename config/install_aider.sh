3de749ce12eed720545f56d5f1f7a6da64ab02b0
Sander Teunissen <s.teunissen@gmail.com>
2025-09-14 12:38:53 +0200

    feat: add comment to use create_symlink.sh script for aider config

--- config/install_aider.sh
+++ config/install_aider.sh
@@ -9,4 +9,5 @@ echo "$PARENT_FOLDER"
 
 uv tool install --force --python python3.12 --with pip aider-chat@latest
 
+#AI! change the below line by using the create_symlink.sh script located inside the $PARENT_FOLDER
 [ ! -e ~/.aider.conf.yml ] && ln -s "$PARENT_FOLDER/aider_config/.aider.conf.yml"  ~/.aider.conf.yml
