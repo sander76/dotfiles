7b4160c4ae3e9b670fb175428160d632e9ec22cb
Your Name <you@example.com>
2025-11-12 11:10:46 +0100

    feat: add wezterm configuration file

---
+++ config/wezterm/.wezterm.lua
@@ -0,0 +1,29 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+-- General
+
+config.font_size=11
+config.line_height=1
+config.color_scheme="tokyonight_night"
+
+config.colors= {
+  cursor_bg="#7aa2f7",cursor_border="#7aa2f7"}
+config.use_fancy_tab_bar=true
+
+#AI! create a hyperlink from a file which has an extension of ".p"
+
+wezterm.GLOBAL.os = wezterm.GLOBAL.os or
+  string.find(wezterm.target_triple, '-windows-') and 'windows' or
+  string.find(wezterm.target_triple, '-apple-') and 'macos' or
+  string.find(wezterm.target_triple, '-linux-') and 'linux' or
+  error('Unsupported Operating System')
+
+
+
+
+if wezterm.GLOBAL.os == 'windows' then
+  config.default_domain = 'WSL:Ubuntu-24.04'
+end
+
+return config
