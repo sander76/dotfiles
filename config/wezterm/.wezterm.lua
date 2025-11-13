5e33fd5fa90839f7d5b02d8ea8df6c312397248c
Your Name <you@example.com>
2025-11-13 13:29:27 +0100

    update wezterm

--- config/wezterm/.wezterm.lua
+++ config/wezterm/.wezterm.lua
@@ -33,9 +33,54 @@ wezterm.GLOBAL.os = wezterm.GLOBAL.os or
 
 
 
-
 if wezterm.GLOBAL.os == 'windows' then
   config.default_domain = 'WSL:Ubuntu-24.04'
 end
+
+config.audible_bell = "Disabled"
+
+-- Window dressing.
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+
+tabline.setup({
+  options = {
+    icons_enabled = true,
+    theme = "tokyonight_night",
+    tabs_enabled = true,
+    theme_overrides = {},
+    section_separators = {
+      left = wezterm.nerdfonts.pl_left_hard_divider,
+      right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+    component_separators = {
+      left = wezterm.nerdfonts.pl_left_soft_divider,
+      right = wezterm.nerdfonts.pl_right_soft_divider,
+    },
+    tab_separators = {
+      left = wezterm.nerdfonts.pl_left_hard_divider,
+      right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+  },
+  sections = {
+    tabline_a = {},
+    tabline_b = {},
+    tabline_c = { },
+    tab_active = {
+      'index',
+      { 'parent', padding = 0 },
+      '/',
+      { 'cwd', padding = { left = 0, right = 1 } },
+      { 'zoomed', padding = 0 },
+    },
+    tab_inactive = { 'index', { 'process', padding = { left = 0, right = 1 } } },
+    tabline_x = {},
+    tabline_y = {},
+    tabline_z = {},
+  },
+  extensions = {},
+})
+
+tabline.apply_to_config(config)
+
 
 return config
