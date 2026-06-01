79146c3e13357d88b2ba7681a71e3c75c49986b4
Sander Teunissen <s.teunissen@gmail.com>
2026-06-01 10:42:46 +0200

    oil config

--- stow/.config/nvim/lua/plugins/oil.lua
+++ stow/.config/nvim/lua/plugins/oil.lua
@@ -26,5 +26,24 @@ return {
         border = "rounded",
       },
     },
+    init = function()
+      -- When oil deletes a file, wipe any open buffer for that file.
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "OilActionsPost",
+        callback = function(event)
+          if event.data.err then return end
+          for _, action in ipairs(event.data.actions) do
+            if action.type == "delete" and action.entry_type == "file" then
+              -- Oil URLs are like "oil:///absolute/path"; strip the scheme.
+              local path = action.url:gsub("^oil://", "")
+              local bufnr = vim.fn.bufnr(path)
+              if bufnr ~= -1 then
+                vim.api.nvim_buf_delete(bufnr, { force = true })
+              end
+            end
+          end
+        end,
+      })
+    end,
   },
 }
