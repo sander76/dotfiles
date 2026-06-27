0d847b3e4fbd1cb16d5a80bee85d57ef6bfc432d
Sander Teunissen <s.teunissen@gmail.com>
2026-06-27 12:10:37 +0200

    update install kanata

--- config/install_kanata.sh
+++ config/install_kanata.sh
@@ -13,9 +13,6 @@ if [ ! -d ~/.config/systemd/user ]; then
     mkdir -p ~/.config/systemd/user
 fi
 
-sudo "$PARENT_FOLDER/create_symlink.sh" "$PARENT_FOLDER/kanata_service/kanata.service" /lib/systemd/system/kanata.service
-"$PARENT_FOLDER/create_symlink.sh" "$PARENT_FOLDER/kanata" ~/.config/kanata
-
 sudo systemctl daemon-reload
 sudo systemctl start kanata.service
 sudo systemctl enable kanata.service
