f39fcc9ce9de764c69bca0144cc3f97f40d035ad
Sander Teunissen <s.teunissen@gmail.com>
2026-09-24 08:44:58 +0200

    add navigation even when in insert mode

--- stow/.config/nvim/lua/plugins/tmux-navigator.lua
+++ stow/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -14,5 +14,10 @@ return {
     { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>",       desc = "Navigate up" },
     { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>",    desc = "Navigate right" },
     { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", desc = "Navigate previous" },
+    -- Insert mode: leave insert mode first, then navigate.
+    { "<c-h>", "<Esc><cmd>TmuxNavigateLeft<cr>",     mode = "i", desc = "Navigate left" },
+    { "<c-j>", "<Esc><cmd>TmuxNavigateDown<cr>",     mode = "i", desc = "Navigate down" },
+    { "<c-k>", "<Esc><cmd>TmuxNavigateUp<cr>",       mode = "i", desc = "Navigate up" },
+    { "<c-l>", "<Esc><cmd>TmuxNavigateRight<cr>",    mode = "i", desc = "Navigate right" },
   },
 }
